699198392d0262d852f21462885451a5db6e11e6
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-31 14:42:17 +0900

    .

--- exam01/src/member/containers/JoinContainer.tsx
+++ exam01/src/member/containers/JoinContainer.tsx
@@ -3,17 +3,24 @@ import { useState } from 'react'
 import JoinForm from '../components/JoinForm'
 
 type FormType = {
-  email: string
-  password: string
-  confirmPassword: string
-  name: string
+  email?: string
+  password?: string
+  confirmPassword?: string
+  name?: string
+  agree?: boolean
 }
 
 const JoinContainer = (): React.ReactNode => {
-  const [form, setForm] = useState<FormType>()
+  const [form, setForm] = useState<FormType>({ email: 'user01@test.org' })
 
   const handleChange = (e) => {
-    
+    // form[e.target.name] =  e.target.value;
+    // setForm(form)
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleToggle = () => {
+    setForm({...form, agree: !Boolean(form.agree)})
   }
 
   /*
@@ -22,9 +29,19 @@ const JoinContainer = (): React.ReactNode => {
   const onSubmit = (e) => {
     // form 태그의 기본 동작 차단
     e.preventDefault()
+    
+    /* 
+      양식 검증
+      1) 필수 항목 검증
+      2) 
+    */
   }
 
-  return <JoinForm onSubmit={onSubmit} onChange={handleChange}/>
+  return (
+    <>
+      <JoinForm onSubmit={onSubmit} onChange={handleChange} form={form} onToggle={handleToggle}/>
+    </>
+  )
 }
 
 export default JoinContainer
