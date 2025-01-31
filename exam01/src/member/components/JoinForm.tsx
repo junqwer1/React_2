d0d2e8eb70fa30d26e2485795fc89351f11ab456
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-31 12:55:55 +0900

    .

--- exam01/src/member/components/JoinForm.tsx
+++ exam01/src/member/components/JoinForm.tsx
@@ -1,31 +1,34 @@
-const JoinForm = () : React.ReactNode => {
-  return (<form>
-    <dl>
-      <dt>이메일</dt>
-      <dd>
-        <input type="text" />
-      </dd>
-    </dl>
-    <dl>
-      <dt>비밀번호</dt>
-      <dd>
-        <input type="password" />
-      </dd>
-    </dl>
-    <dl>
-      <dt>비밀번호 확인</dt>
-      <dd>
-        <input type="password" />
-      </dd>
-    </dl>
-    <dl>
-      <dt>회원명</dt>
-      <dd>
-        <input type="text" />
-      </dd>
-    </dl>
-    <button type="submit">가입하기</button>
-  </form>)
+'use client'
+const JoinForm = ({ onSubmit, onChange }) : React.ReactNode => {
+  return (
+    <form onSubmit={onSubmit}>
+      <dl>
+        <dt>이메일</dt>
+        <dd>
+          <input type="text" name="email" onChange={onChange}/>
+        </dd>
+      </dl>
+      <dl>
+        <dt>비밀번호</dt>
+        <dd>
+          <input type="password" name="password" onChange={onChange}/>
+        </dd>
+      </dl>
+      <dl>
+        <dt>비밀번호 확인</dt>
+        <dd>
+          <input type="password" name="confirmPassword" onChange={onChange}/>
+        </dd>
+      </dl>
+      <dl>
+        <dt>회원명</dt>
+        <dd>
+          <input type="text" name="name" onChange={onChange}/>
+        </dd>
+      </dl>
+      <button type="submit">가입하기</button>
+    </form>
+  )
 }
 
 export default JoinForm
--- exam01/src/member/containers/JoinContainer.tsx
+++ exam01/src/member/containers/JoinContainer.tsx
@@ -1,7 +1,30 @@
-import JoinForm from "../components/JoinForm"
-const JoinContainer = (): React.ReactNode => {
+'use client'
+import { useState } from 'react'
+import JoinForm from '../components/JoinForm'
 
-  return <JoinForm />
+type FormType = {
+  email: string
+  password: string
+  confirmPassword: string
+  name: string
+}
+
+const JoinContainer = (): React.ReactNode => {
+  const [form, setForm] = useState<FormType>()
+
+  const handleChange = (e) => {
+    
+  }
+
+  /*
+  양식 제출시 처리 함수
+  */
+  const onSubmit = (e) => {
+    // form 태그의 기본 동작 차단
+    e.preventDefault()
+  }
+
+  return <JoinForm onSubmit={onSubmit} onChange={handleChange}/>
 }
 
 export default JoinContainer
