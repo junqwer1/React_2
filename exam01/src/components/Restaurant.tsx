ea5a966b2b4703f8df34344bcfda6acff296e4cf
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-31 10:43:20 +0900

    .

--- exam01/src/components/Restaurant.tsx
+++ exam01/src/components/Restaurant.tsx
@@ -1,20 +1,42 @@
 'use client'
 import { useLayoutEffect, useState } from 'react'
 
+type RestaurantType = {
+  seq: number
+  category: string
+  address: string
+  name: string
+  latitude: number
+  longitude: number
+}
 
 const Restaurant = () => {
-  const [limit, setLimit] = useState<number> (10)
-  const [items, setItems] = useState({})
+  const [limit, setLimit] = useState<number>(10)
+  const [items, setItems] = useState<RestaurantType | undefined>()
   useLayoutEffect(() => {
     const url = `http://restaurant-service.koreait.xyz/restaurant/search?lat=37.5106297&lon=126.7246897&limit=${limit}`
     ;(async () => {
       const res = await fetch(url)
       const items = await res.json()
-      console.log(items)
+      setItems(items)
     })()
-  }, [])
+  }, [items, limit])
 
-  return <></>
+  return (
+    <ul>
+      {items && items.map((item) => <RestaurantItem key={item.seq} item={item} />)}
+    </ul>
+  )
+}
+
+const RestaurantItem = ({ item }): React.ReactNode => {
+  const { category, name } = item
+  return (
+    <li>
+      <span>{category}</span>
+      <span>{name}</span>
+    </li>
+  )
 }
 
 export default Restaurant
