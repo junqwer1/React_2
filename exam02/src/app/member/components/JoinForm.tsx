4b88141ccedee2018a6851b31df59cdbe558cfd2
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-02-03 11:48:51 +0900

    .

--- exam02/src/app/member/components/JoinForm.tsx
+++ exam02/src/app/member/components/JoinForm.tsx
@@ -1,32 +1,38 @@
-const JoinForm = () => {
+'use client'
+const JoinForm = ({actionState}) => {
+  const [errors/* 리턴 값 */, formAction, isPending/*<- 중복제출 방지 */] = actionState
   return (
     <>
-      <form>
+      <form action={formAction}>
         <dl>
           <dt>이메일</dt>
           <dd>
             <input type="text" name="email" />
+            {errors?.email && errors.email.map(m => <div key={m}>{m}</div>)}
           </dd>
         </dl>
         <dl>
           <dt>비밀번호</dt>
           <dd>
             <input type="password" name="password" />
+            {errors?.password && errors.password.map(m => <div key={m}>{m}</div>)}
           </dd>
         </dl>
         <dl>
           <dt>비밀번호 확인</dt>
           <dd>
             <input type="password" name="confirmPassword" />
+            {errors?.confirmPassword && errors.confirmPassword.map(m => <div key={m}>{m}</div>)}
           </dd>
         </dl>
         <dl>
           <dt>회원명</dt>
           <dd>
             <input type="text" name="name" />
+            {errors?.name && errors.name.map(m => <div key={m}>{m}</div>)}
           </dd>
         </dl>
-        <button type="submit">가입하기</button>
+        <button type="submit" disabled={isPending}>가입하기</button>
       </form>
     </>
   )
--- exam02/src/app/member/containers/JoinContainer.tsx
+++ exam02/src/app/member/containers/JoinContainer.tsx
@@ -1,8 +1,20 @@
+'use client'
 import JoinForm from '../components/JoinForm'
 import { processJoin } from '../service/actions'
+import { useActionState, useState } from 'react'
+
+type FormType = {
+  email?: string
+  password?: string
+  comfirmPassword?: string
+  name?: string
+  agree?: boolean
+}
 
 const JoinContainer = () => {
-  return <JoinForm formAction={processJoin} />
+  const { form, setForm } = useState<FormType>({})
+  const actionState = useActionState(processJoin, {})
+  return <JoinForm actionState={actionState} />
 }
 
 export default JoinContainer
