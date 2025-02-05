ceeb71af298643615e0e43eb822a7e8c9bdbe055
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-02-05 10:42:49 +0900

    .

---
+++ exam02/src/app/exam03/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { useState } from 'react'
+
+function getTotal(numbers: number[]) {
+  return numbers.reduce((a, b) => a + b)
+}
+
+const Exam03Page = () => {
+  const [numbers, setNumbers] = useState<number[]>([])
+  const [number, setNumber] = useState<number | undefined>()
+
+  const handleChage = (e) => setNumber(e.target.value)
+  const handleClick = () => {
+    if (number && !isNaN(number)) {
+      setNumbers(numbers.concat(number))
+    }
+  }
+
+  return (
+    <>
+      <input type="number" onChange={handleChage} />
+      <button type="button" onClick={handleClick}>
+        더하기
+      </button>
+      <ul>
+        {numbers.map((num, i) => (
+          <li key={num + '_' + i}>{num}</li>
+        ))}
+      </ul>
+      <div>합계: {getTotal(numbers)}</div>
+    </>
+  )
+}
+
+export default Exam03Page
